7027b912480158b65c5a5240f3e9f6baef56babf
AmiyaSatyam <amiyasatyam@gmail.com>
2019-06-23 14:17:36 +0530

    Initial commit

---
+++ Source/EnergyCollector/PickUp.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PickUp.generated.h"
+
+UCLASS()
+class ENERGYCOLLECTOR_API APickUp : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickUp();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	bool bIsActive;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** Return the mesh for the pickup */
+	FORCEINLINE class UStaticMeshComponent* GetMesh() const { return PickupMesh; }
+
+	UFUNCTION(BlueprintPure, Category = "Pickup")
+	bool IsActive();
+
+	UFUNCTION(BlueprintCallable, Category = "Pickup")
+	void setActive(bool NewPickupState);
+
+private:
+	/**static mesh to represent the pickup in the level*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup", meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* PickupMesh;
+
+};
