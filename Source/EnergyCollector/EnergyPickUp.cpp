a88b1fa4037ad957253ca6e4fcdb68764c10fd0c
AmiyaSatyam <amiyasatyam@gmail.com>
2019-06-23 16:39:01 +0530

    WhereToPickup

---
+++ Source/EnergyCollector/EnergyPickUp.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EnergyPickUp.h"
+
+AEnergyPickUp::AEnergyPickUp()
+{
+	GetMesh()->SetSimulatePhysics(true);
+}
---
+++ Source/EnergyCollector/EnergyPickUp.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickUp.h"
+#include "EnergyPickUp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENERGYCOLLECTOR_API AEnergyPickUp : public APickUp
+{
+	GENERATED_BODY()
+
+public:
+	AEnergyPickUp();
+	
+};
